f2c05f13c86316b32d9e42ac77b49145fd99a7bd
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-11-30 04:19:07 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -11,7 +11,7 @@ export default defineConfig({
   base: '/',
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(new URL('/src', import.meta.url))
     }
   }
 })
