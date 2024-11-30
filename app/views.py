1defc00a42b984e4767438e3259f18c9b2a46153
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-12-01 00:23:14 +0300

    Update views.py
    Add views

--- app/views.py
+++ app/views.py
@@ -1,4 +1,56 @@
-from django.http import HttpResponse
+import requests
+from django.http import JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-def index(request):
-    return HttpResponse("<h4>AneCode</h4>")
+
+EXTERNAL_API_URL = "http://147.45.254.226:8889/auth"
+
+@api_view(['POST'])
+def login_view(request):
+
+
+  
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    payload = {
+        'username': username,
+        'password': password
+    }
+
+    try:
+        response = requests.post(f"{EXTERNAL_API_URL}/login", data=payload)
+
+        if response.status_code == 200:
+            return JsonResponse(response.json(), status=200) 
+        else:
+            return JsonResponse({"error": "Invalid credentials or server error"}, status=response.status_code)
+
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": str(e)}, status=500)
+
+
+
+@api_view(['POST'])
+def register_view(request):
+    username = request.data.get('username')
+    password = request.data.get('password')
+
+    payload = {
+        'username': username,
+        'password': password
+    }
+
+
+    try:
+        response = requests.post(f"{EXTERNAL_API_URL}/register", data=payload)
+
+
+        if response.status_code == 201:
+            return JsonResponse({"message": "User registered successfully"}, status=201)
+        else:
+            return JsonResponse({"error": "Registration failed"}, status=response.status_code)
+
+    except requests.exceptions.RequestException as e:
+        return JsonResponse({"error": str(e)}, status=500)
