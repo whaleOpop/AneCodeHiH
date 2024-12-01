a8975025acb03ac261b37af221be394937cee665
Alexey Kuleshov <149209268+AlexxxeyKuleshov@users.noreply.github.com>
2024-12-01 07:53:29 +0300

    Create HelpHeaderComponent.vue

---
+++ src/components/HelpHeaderComponent.vue
@@ -0,0 +1,25 @@
+<template>
+	<header class="header">
+		<h1>Помощь</h1>
+		<p>Ответы на часто задаваемые вопросы</p>
+	</header>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped>
+.header {
+	padding: 20px;
+	background: #f9fafb;
+}
+.header h1 {
+	font-size: 24px;
+	margin: 0;
+}
+.header p {
+	font-size: 16px;
+	color: #7a7a7a;
+}
+</style>
