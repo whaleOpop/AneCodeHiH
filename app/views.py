a3a8103223ce93ab4fbcbe9be62823287c5be95c
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-12-01 06:08:25 +0300

    Update views.py

--- app/views.py
+++ app/views.py
@@ -4,13 +4,10 @@ from rest_framework.decorators import api_view
 from rest_framework.response import Response
 
 
-EXTERNAL_API_URL = "http://147.45.254.226:8889/auth"
+EXTERNAL_API_URL = 'http://147.45.254.226:8889'
 
 @api_view(['POST'])
 def login_view(request):
-
-
-  
     username = request.data.get('username')
     password = request.data.get('password')
 
@@ -20,16 +17,17 @@ def login_view(request):
     }
 
     try:
-        response = requests.post(f"{EXTERNAL_API_URL}/login", data=payload)
+        response = requests.post(f"{EXTERNAL_API_URL}/auth/login", data=payload)
 
         if response.status_code == 200:
-            return JsonResponse(response.json(), status=200) 
+            # Форматируем ответ в соответствии с ожидаемым форматом
+            formatted_response = {
+                "access_token": response.json().get("access_token"),
+                "token_type": response.json().get("token_type")
+            }
+            return JsonResponse(formatted_response, status=200)
         else:
             return JsonResponse({"error": "Invalid credentials or server error"}, status=response.status_code)
 
     except requests.exceptions.RequestException as e:
         return JsonResponse({"error": str(e)}, status=500)
-
-#ф-ция получения всех id 
-#создание отчета
-#ф-ция создание всего отчета
