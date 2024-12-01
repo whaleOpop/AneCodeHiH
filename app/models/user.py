a5c12156e10ef11530171a0b33e2cef8eb6f5db1
WhaleOpop <92190689+whaleOpop@users.noreply.github.com>
2024-12-01 04:40:35 +0300

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -7,5 +7,4 @@ class User(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     username = Column(String, unique=True, index=True)
-    email = Column(String, unique=True, index=True)
     password = Column(String)
